5289900418244980ded9cb0f3c4f16d133428338
Francis Rodrigues <francis.rod01@outlook.com>
2021-03-27 22:55:50 -0300

    wip: Added helmet through app initialization

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import helmet from 'helmet';
 
 import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
+
+// helmet setup
+app.use(helmet());
 
 // mongoose connection
 mongoose.Promise = global.Promise;
