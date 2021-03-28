68294a6c9fa84f91909c88e0a658c67cb187fe7f
Francis Rodrigues <francis.rod01@outlook.com>
2021-03-27 23:17:05 -0300

    wip: rate limiting against DoS attacks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import RateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 
 import routes from './src/routes/crmRoutes';
@@ -10,6 +11,13 @@ const PORT = 3000;
 
 // helmet setup
 app.use(helmet());
+
+// Rate limit setup
+const limiter = RateLimit({
+  windowMs: 15*60*1000, // 15 minutes
+  max: 100, // limit of number of requests fer IP
+});
+app.use(limiter);
 
 // mongoose connection
 mongoose.Promise = global.Promise;
